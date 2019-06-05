87d8d5738c261b091f220735f17ac5bf617d0a3f
X1-001 <tomhaeldermans94@gmail.com>
2019-06-05 22:19:00 +0200

    Merge branch 'master' of https://github.com/ChristoffelsJ/Productviewer
    # Conflicts:
    #	src/util/DBUtil.java

--- src/main/PopupAddProductController.java
+++ src/main/PopupAddProductController.java
@@ -63,7 +63,7 @@ public class PopupAddProductController {
 
     @FXML
     private void addProduct(ActionEvent actionEvent) throws ClassNotFoundException, SQLException, IOException {
-        model.ProductDAO.addProduct(productTitle.getText(),Integer.parseInt(subCategory.getValue()), Integer.parseInt(mainCategory.getValue()), price.getText(), description.getText(), imagePath,0);
+        model.ProductDAO.addProduct(productTitle.getText(),subCategory.getValue(), mainCategory.getValue(), price.getText(), description.getText(), imagePath,0);
         Stage stage = (Stage) addProductButton.getScene().getWindow();
         stage.close();
 
