0c360e2708480fecab225718a8b1f6570db90f87
jonasvolders <jonasvolders1@gmail.com>
2019-06-07 10:27:54 +0200

    les3/6 jonas

---
+++ src/model/ErrorClass.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ErrorClass {
+    private static String Errormessage;
+
+    public static String getErrormessage() {
+        return Errormessage;
+    }
+
+    public static void setErrormessage(String errormessage) {
+        Errormessage = errormessage;
+    }
+}
